06ded74681e2283dbc201246cfcc696c58aed286
Haxrox <56497846+Haxrox@users.noreply.github.com>
2024-06-08 16:06:22 -0700

    refactored colours

--- bash_lib/.bash_lib/prompt_lib/colours.sh
+++ bash_lib/.bash_lib/prompt_lib/colours.sh
@@ -1,75 +1,75 @@
 #!/bin/bash
-# Credits to: https://github.com/slomkowski/bash-full-of-colors/blob/master/bashrc.sh
+# CREDits to: https://github.com/slomkowski/bash-full-of-colors/blob/master/bashrc.sh
 
 # Reset
-Color_Off='\e[0m'       # Text Reset
+NO_COLOR='\e[0m'       # Text Reset
 
 # Regular Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
+BLACK='\e[0;30m'        # BLACK
+RED='\e[0;31m'          # RED
+GREEN='\e[0;32m'        # GREEN
+YELLOW='\e[0;33m'       # YELLOW
+BLUE='\e[0;34m'         # BLUE
+PURPLE='\e[0;35m'       # PURPLE
+CYAN='\e[0;36m'         # CYAN
+WHITE='\e[0;37m'        # WHITE
 
 # Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
+B_BLACK='\e[1;30m'       # BLACK
+B_RED='\e[1;31m'         # RED
+B_GREEN='\e[1;32m'       # GREEN
+B_YELLOW='\e[1;33m'      # YELLOW
+B_BLUE='\e[1;34m'        # BLUE
+B_PURPLE='\e[1;35m'      # PURPLE
+B_CYAN='\e[1;36m'        # CYAN
+B_WHITE='\e[1;37m'       # WHITE
 
 # Underline
-UBlack='\e[4;30m'       # Black
-URed='\e[4;31m'         # Red
-UGreen='\e[4;32m'       # Green
-UYellow='\e[4;33m'      # Yellow
-UBlue='\e[4;34m'        # Blue
-UPurple='\e[4;35m'      # Purple
-UCyan='\e[4;36m'        # Cyan
-UWhite='\e[4;37m'       # White
+U_BLACK='\e[4;30m'       # BLACK
+U_RED='\e[4;31m'         # RED
+U_GREEN='\e[4;32m'       # GREEN
+U_YELLOW='\e[4;33m'      # YELLOW
+U_BLUE='\e[4;34m'        # BLUE
+U_PURPLE='\e[4;35m'      # PURPLE
+U_CYAN='\e[4;36m'        # CYAN
+U_WHITE='\e[4;37m'       # WHITE
 
 # Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Purple='\e[45m'      # Purple
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
+ON_BLACK='\e[40m'       # BLACK
+ON_RED='\e[41m'         # RED
+ON_GREEN='\e[42m'       # GREEN
+ON_YELLOW='\e[43m'      # YELLOW
+ON_BLUE='\e[44m'        # BLUE
+ON_PURPLE='\e[45m'      # PURPLE
+ON_CYAN='\e[46m'        # CYAN
+ON_WHITE='\e[47m'       # WHITE
 
 # High Intensity
-IBlack='\e[0;90m'       # Black
-IRed='\e[0;91m'         # Red
-IGreen='\e[0;92m'       # Green
-IYellow='\e[0;93m'      # Yellow
-IBlue='\e[0;94m'        # Blue
-IPurple='\e[0;95m'      # Purple
-ICyan='\e[0;96m'        # Cyan
-IWhite='\e[0;97m'       # White
+I_BLACK='\e[0;90m'       # BLACK
+I_RED='\e[0;91m'         # RED
+I_GREEN='\e[0;92m'       # GREEN
+I_YELLOW='\e[0;93m'      # YELLOW
+I_BLUE='\e[0;94m'        # BLUE
+I_PURPLE='\e[0;95m'      # PURPLE
+I_CYAN='\e[0;96m'        # CYAN
+I_WHITE='\e[0;97m'       # WHITE
 
 # Bold High Intensity
-BIBlack='\e[1;90m'      # Black
-BIRed='\e[1;91m'        # Red
-BIGreen='\e[1;92m'      # Green
-BIYellow='\e[1;93m'     # Yellow
-BIBlue='\e[1;94m'       # Blue
-BIPurple='\e[1;95m'     # Purple
-BICyan='\e[1;96m'       # Cyan
-BIWhite='\e[1;97m'      # White
+BI_BLACK='\e[1;90m'      # BLACK
+BI_RED='\e[1;91m'        # RED
+BI_GREEN='\e[1;92m'      # GREEN
+BI_YELLOW='\e[1;93m'     # YELLOW
+BI_BLUE='\e[1;94m'       # BLUE
+BI_PURPLE='\e[1;95m'     # PURPLE
+BI_CYAN='\e[1;96m'       # CYAN
+BI_WHITE='\e[1;97m'      # WHITE
 
 # High Intensity backgrounds
-On_IBlack='\e[0;100m'   # Black
-On_IRed='\e[0;101m'     # Red
-On_IGreen='\e[0;102m'   # Green
-On_IYellow='\e[0;103m'  # Yellow
-On_IBlue='\e[0;104m'    # Blue
-On_IPurple='\e[0;105m'  # Purple
-On_ICyan='\e[0;106m'    # Cyan
-On_IWhite='\e[0;107m'   # White
+ON_IBLACK='\e[0;100m'   # BLACK
+ON_IRED='\e[0;101m'     # RED
+ON_IGREEN='\e[0;102m'   # GREEN
+ON_IYELLOW='\e[0;103m'  # YELLOW
+ON_IBLUE='\e[0;104m'    # BLUE
+ON_IPURPLE='\e[0;105m'  # PURPLE
+ON_ICYAN='\e[0;106m'    # CYAN
+ON_IWHITE='\e[0;107m'   # WHITE
