86ac45a199bf066b9dd5ad25a7423015b845c246
Haxrox <56497846+Haxrox@users.noreply.github.com>
2024-06-08 13:24:48 -0700

    updated install logs

--- bash_lib/install.sh
+++ bash_lib/install.sh
@@ -9,27 +9,27 @@ BASH_PERSONAL="${BASH_LIB}/bash_personal"
 SOURCE_BASH_PERSONAL="source ${TARGET_ROOT}/${BASH_PERSONAL}"
 
 if (( ${DEBUG} )); then
-  echo "[UBUNTU] BASHRC: ${BASHRC}"
-  echo "[UBUNTU] TARGET_ROOT: ${TARGET_ROOT}"
-  echo "[UBUNTU] BASH_PERSONAL: ${BASH_PERSONAL}"
-  echo "[UBUNTU] SOURCE_BASH_PERSONAL: ${SOURCE_BASH_PERSONAL}"
+  echo "[HAXUBUNTU] BASHRC: ${BASHRC}"
+  echo "[HAXUBUNTU] TARGET_ROOT: ${TARGET_ROOT}"
+  echo "[HAXUBUNTU] BASH_PERSONAL: ${BASH_PERSONAL}"
+  echo "[HAXUBUNTU] SOURCE_BASH_PERSONAL: ${SOURCE_BASH_PERSONAL}"
 fi
 
 # Check if line has been sourced yet
 if ! (cat "${BASHRC}" | grep -q "^${SOURCE_BASH_PERSONAL}"); then
-  echo "[UBUNTU] Adding '${SOURCE_BASH_PERSONAL}' to ${BASHRC}"
+  echo "[HAXUBUNTU] Adding '${SOURCE_BASH_PERSONAL}' to ${BASHRC}"
 
   if (( DEBUG )); then
-    echo "[UBUNTU] Debug | Appending ${SOURCE_BASH_PERSONAL} to ${BASHRC}"
+    echo "[HAXUBUNTU] Debug | Appending ${SOURCE_BASH_PERSONAL} to ${BASHRC}"
   else
     echo -e "\n${SOURCE_BASH_PERSONAL}" >> "${BASHRC}"
   fi
 fi
 
-echo "[UBUNTU] Updating ${BASH_LIB} contents"
+echo "[HAXUBUNTU] Updating ${BASH_LIB} contents"
 
 if (( ${DEBUG} )); then
-  echo "[UBUNTU] Debug | Copying ${BASH_LIB} into ${TARGET_ROOT}"
+  echo "[HAXUBUNTU] Debug | Copying ${BASH_LIB} into ${TARGET_ROOT}"
 else
   cp -r ${BASH_LIB} ${TARGET_ROOT}
 fi
