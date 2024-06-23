c164beeebf6261d888d233e42af3cfc651522eff
Haxrox <56497846+Haxrox@users.noreply.github.com>
2024-06-22 19:23:30 -0700

    moved bash prompt set to prompt_lib

--- bash_lib/.bash_lib/prompt_lib/prompt_lib.sh
+++ bash_lib/.bash_lib/prompt_lib/prompt_lib.sh
@@ -4,21 +4,28 @@ source "$(dirname "${BASH_SOURCE[0]}")/colours.sh"
 
 PROMPT_PREPEND=""
 PROMPT_APPEND=""
+DEFAULT_PROMPT="\[$(echo '${PROMPT_PREPEND}')${B_GREEN}\][\@]\[${RED}$(echo '$(__git_ps1 ["%s"])')$(echo '${PROMPT_APPEND}')${B_BLUE}\]\w/\[${NO_COLOR}\]$ "
 
 function setPrompt() {
   local ps1="$1"
 
-  PS1="${ps1}";
+  PS1="${ps1}"
 }
 
 function promptPrepend() {
   local prepend="$1"
 
-  PROMPT_PREPEND+="${prepend}";
+  PROMPT_PREPEND+="${prepend}"
 }
 
 function promptAppend() {
   local append="$1"
 
-  PROMPT_APPEND+="${append}";
+  PROMPT_APPEND+="${append}"
 }
+
+function main() {
+  setPrompt "${DEFAULT_PROMPT}"
+}
+
+main $@
